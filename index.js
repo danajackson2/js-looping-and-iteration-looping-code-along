cd6af2a0967c92c88a579d87c1b0abd34e60e0a3
danajackson2 <danajackson2@gmail.com>
2021-01-11 11:47:50 -0800

    weird tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(arr, event){
+    for (let i=0, i < arr.length, i++){
+        console.log(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+}
+
+function countDown(num){
+    let i = num
+    while (i > 0){
+        console.log(num)
+        i--
+    }
+}
